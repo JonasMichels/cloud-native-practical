91b546c91f4657e862d340cc6ae22cc071781518
Jonas Michels <jonas.michels@kbc.be>
2020-06-09 13:51:34 +0200

    lab 04 - unit test working

--- ezGroceries/src/test/java/com/ezgroceries/shoppinglist/web/controller/CocktailControllerTest.java
+++ ezGroceries/src/test/java/com/ezgroceries/shoppinglist/web/controller/CocktailControllerTest.java
@@ -1,22 +1,18 @@
 package com.ezgroceries.shoppinglist.web.controller;
 
-import static com.ezgroceries.shoppinglist.web.controller.ShoppingListControllerTest.asJsonString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import com.ezgroceries.shoppinglist.cocktail.controllers.CocktailController;
-import com.ezgroceries.shoppinglist.cocktail.resources.CocktailDBClient;
 import com.ezgroceries.shoppinglist.cocktail.resources.CocktailDBResponse.DrinkResource;
 import com.ezgroceries.shoppinglist.cocktail.resources.CocktailResource;
-import com.ezgroceries.shoppinglist.cocktail.service.CocktailService;
 import com.ezgroceries.shoppinglist.shoppingList.controllers.ShoppingListController;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
@@ -33,14 +29,15 @@ public class CocktailControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
-    private CocktailService cocktailService;
+    private CocktailController cocktailController;
 
     @Test
     public void getCocktails() throws Exception {
+        String searchany = "All";
         List<CocktailResource> cocktails = new ArrayList<>();
         DrinkResource drinkResource = new DrinkResource();
         drinkResource.setIdDrink("23b3d85a-3928-41c0-a533-6538a71e17c4");
-        drinkResource.setStrDrink("Blue margarita");
+        drinkResource.setStrDrink("margarita");
         drinkResource.setStrGlass("Cocktail Glass");
         drinkResource.setStrDrinkThumb("https://www.thecocktaildb.com/images/media/drink/qtvvyq1439905913.jpg");
         drinkResource.setStrInstructions("Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten..");
@@ -50,18 +47,32 @@ public class CocktailControllerTest {
         drinkResource.setStrIngredient4("Salt");
         CocktailResource cocktail1 = new CocktailResource(drinkResource);
         cocktails.add(cocktail1);
+        DrinkResource drinkResource2 = new DrinkResource();
+        drinkResource2.setIdDrink("d615ec78-fe93-467b-8d26-5d26d8eab073");
+        drinkResource2.setStrDrink( "Blue Margerita");
+        drinkResource2.setStrGlass("Cocktail glass");
+        drinkResource2.setStrInstructions("Rub rim of cocktail glass with lime juice. Dip rim in coarse salt..");
+        drinkResource2.setStrDrinkThumb("https://www.thecocktaildb.com/images/media/drink/qtvvyq1439905913.jpg");
+        drinkResource2.setStrIngredient1("Tequila");
+        drinkResource2.setStrIngredient2("Blue Curacao");
+        drinkResource2.setStrIngredient3("Lime juice");
+        drinkResource2.setStrIngredient4("Salt");
+        CocktailResource cocktail2 = new CocktailResource(drinkResource2);
+        cocktails.add(cocktail2);
 
 
 
-        given(cocktailService.getCocktails(any(String.class)))
+        given(cocktailController.get(any(String.class)))
                 .willReturn(cocktails);
 
 
         mockMvc.perform(get("/cocktails")
-                .contentType(MediaType.APPLICATION_JSON))
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("search",searchany))
+        .andDo(print())
         .andExpect(status().isOk());
 
-        verify(cocktailService.getCocktails(any(String.class)));
+        verify(cocktailController).get(any(String.class));
     }
 
 }
